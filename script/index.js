d727bb0877265f56b8f0212431f042c434ff1b7d
leor <leo442183205@gmail.com>
2019-01-27 20:40:28 -0800

    shopping cart construction for two items

--- script/index.js
+++ script/index.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   var cart = {};
+  var cartCount = 0;
 
-  $('.button, .button-purchase').on('click', function(event) {
+
+  // For Rad Dad
+  // For the purpose and the scale of this project I will have multiple click event for each item
+  $('.rad_dad').on('click', function(event) {
     event.preventDefault();
 
     var delivery_method = $('input[name=option1]:checked').val();
     var quantity = Number($('input[name=quantity]').val());
+    var price = 3.99;
     var msg;
 
     // If user did not choose a deliver method
@@ -14,9 +19,70 @@ $(document).ready(function() {
     }
 
     if(delivery_method === 'Sign') {
-      msg = $('.paragraph-text').val();
+      price += 1.99;
+      msg = $('.dad_text').val();
     }
 
-    console.log(delivery_method);
+    // Fixing decemal places to 2 for display
+    price = (price * quantity).toFixed(2);
+
+    // Resetting selected fields
+    $('.dad_text').val("");
+    // Radio buttons reset code from https://stackoverflow.com/questions/2554116/how-to-clear-radio-button-in-javascript
+    $('input[name=option1]:checked')[0].checked = false;
+    $('input[name=quantity]').val(1);
+
+    cart[cartCount] = {
+      Item: "Rad Dad",
+      Price: "3.99",
+      Quantity: quantity,
+      Total: price,
+      Message: msg,
+      Delivery: delivery_method
+    }
+
+    console.log(cart);
+    cartCount++;
+  });
+
+
+  $('.better_friend').on('click', function(event) {
+    event.preventDefault();
+
+    var delivery_method = $('input[name=option]:checked').val();
+    var quantity = Number($('input[name=quantity2]').val());
+    var price = 6.99;
+    var msg;
+
+    // If user did not choose a deliver method
+    if(!delivery_method) {
+      alert("ERROR: You did not choose a delivery method.");
+    }
+
+    if(delivery_method === 'Sign') {
+      price += 1.99;
+      msg = $('.friend_text').val();
+    }
+
+    // Fixing decemal places to 2 for display
+    price = (price * quantity).toFixed(2);
+
+     // Resetting selected fields
+    $('.friend_text').val("");
+    // Radio buttons reset code from https://stackoverflow.com/questions/2554116/how-to-clear-radio-button-in-javascript
+    $('input[name=option]:checked')[0].checked = false;
+    $('input[name=quantity2]').val(1);
+
+    cart[cartCount] = {
+      Item: "Couldn't Pick a Better Friend Birthday Card",
+      Price: "6.99",
+      Quantity: quantity,
+      Total: price,
+      Message: msg,
+      Delivery: delivery_method
+    }
+
+    console.log(cart);
+    cartCount++;
   });
 });
